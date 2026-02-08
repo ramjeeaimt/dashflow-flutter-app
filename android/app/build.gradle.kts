938cdfc52c73ab7c20367aad570df065aa991e47
Vicky <ranjeetkumar123@gmail.com>
2026-02-08 18:06:25 +0530

    saved updated

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.flutter_application_difmo"
+    namespace = "com.difmo.dashflow"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -19,22 +19,31 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("../../myapp.keystore")
+            storePassword = "difmo123"  // Replace with your actual keystore password
+            keyAlias = "myappkey"
+            keyPassword = "difmo123"  // Replace with your actual key password
+        }
+    }
+
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_application_difmo"
+        applicationId = "com.difmo.dashflow"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 25
         targetSdk = 35
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode = 1
+        versionName = "1.0.0"
     }
 
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
--- android/app/src/main/kotlin/com/example/flutter_application_difmo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_application_difmo/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_application_difmo
+package com.difmo.dashflow
 
 import io.flutter.embedding.android.FlutterActivity
 
